891109a774b690af1d9837bb98a319ecd32c2542
Nats234 <natashacaroline323@gmail.com>
2024-10-04 18:37:07 +0300

    Add files via upload

---
+++ src/myodds/Myodd.java
@@ -0,0 +1,18 @@
+package myodds;
+import java.util.Scanner;
+
+public class Myodd {
+
+	public static void main(String[] args) {
+		for(int y=1;y<=20;y++) {
+			if(y%2!=0) {
+				System.out.println(y);
+				
+			
+			}
+		}
+		
+
+	}
+
+}
